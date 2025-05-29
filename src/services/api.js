21c585977274f285c8ba7c1e339f87f78c02e7e1
PerseoN15 <andres.nc@tecjerez.edu.mx>
2025-05-28 22:49:56 -0600

    Conectar frontend a backend en Clever Cloud

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,12 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api", // Cambiar cuando tengas backend activo
+  baseURL: "https://backend-tutorias.cleverapps.io/api", 
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-// ✅ Interceptor para agregar token automáticamente a cada solicitud
 API.interceptors.request.use(config => {
   const user = JSON.parse(localStorage.getItem('usuario'));
   if (user?.token) {
